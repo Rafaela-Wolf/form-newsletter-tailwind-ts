e06803dd128a539958ba9fdc9666b3883b87495e
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-03-14 12:10:16 -0300

    estruturando o componente de form

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import Form from './components/Form';
 
 function App() {
   return (
-    <>
+    <div>
       <h1>Inscreva-se</h1>
-      <Form />
-    </>
+      <p>Assine nossa newsletter e mantenha-se informado.</p>
+
+      <div>
+        <Form />
+      </div>
+      
+      <p>
+        Ao se inscrever, você passará a receber os nossos conteúdos, novidades e ofertas.
+      </p>
+    </div>
   )
 }
 
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,34 @@
 const Form = () => {
   return (
-    <div>Form</div>
+    <form>
+      <div>
+        <label>Nome</label>
+        <input 
+          type="text"
+          placeholder="Digite seu nome" 
+        />
+      </div>
+
+      <div>
+        <label>E-mail</label>
+        <input 
+          type="text"
+          placeholder="Insira seu e-mail" 
+        />
+      </div>
+
+      <div>
+        <a href="#">Leia os termos</a>
+        <div>
+          <input 
+            type="checkbox"
+          />
+          <label>Concordo com os termos</label>
+        </div>
+
+        <button type="submit">Cadastrar</button>
+      </div>
+    </form>
   )
 }
 
