91b117c55c9dd5fcf7ac83f5a67bffadcf9e23e0
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-03-14 12:35:57 -0300

    adicionando os estilos principais

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,32 +1,34 @@
 const Form = () => {
   return (
-    <form>
-      <div>
-        <label>Nome</label>
+    <form className="flex flex-col gap-3">
+      <div className="flex flex-col">
+        <label className="text-sm" htmlFor="name">Nome</label>
         <input 
+          className="rounded-lg focus:ring-0 focus:outline-none py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
           type="text"
           placeholder="Digite seu nome" 
         />
       </div>
 
-      <div>
-        <label>E-mail</label>
+      <div className="flex flex-col">
+        <label className="text-sm" htmlFor="email">E-mail</label>
         <input 
+          className="rounded-lg focus:ring-0 focus:outline-none py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
           type="text"
           placeholder="Insira seu e-mail" 
         />
       </div>
 
-      <div>
-        <a href="#">Leia os termos</a>
-        <div>
+      <div className="flex flex-col">
+        <a href="#" className="text-xs underline mb-2">Leia os termos</a>
+        <div className="flex gap-2 items-center">
           <input 
             type="checkbox"
           />
-          <label>Concordo com os termos</label>
+          <label className="text-sm" htmlFor="agree">Concordo com os termos</label>
         </div>
 
-        <button type="submit">Cadastrar</button>
+        <button type="submit" className="bg-slate-600 hover:bg-slate-500 font-medium text-sm py-2 px-4 rounded-lg text-white">Cadastrar</button>
       </div>
     </form>
   )
